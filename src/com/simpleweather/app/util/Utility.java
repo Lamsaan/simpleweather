2c7f5fa996b2fdfc6bf9fc56231bbd6a9d928ac6
Lamsaan <rsamyau@163.com>
2015-11-03 00:18:51 +0800

    完成遍历省市县三级列表的功能。

---
+++ src/com/simpleweather/app/util/Utility.java
@@ -0,0 +1,87 @@
+package com.simpleweather.app.util;
+
+import android.text.TextUtils;
+
+import com.simpleweather.app.db.SimpleWeatherDB;
+import com.simpleweather.app.model.City;
+import com.simpleweather.app.model.County;
+import com.simpleweather.app.model.Province;
+
+public class Utility {
+	
+	public synchronized static boolean handlerProvinceResponse(SimpleWeatherDB simpleWeatherDB,
+			String response){
+		
+		if(!TextUtils.isEmpty(response)){
+			String[] allProvinces =response.split(",");
+			if(allProvinces!=null&&allProvinces.length>0){
+				for(String p:allProvinces){
+					String[] array = p.split("\\|");
+					Province province = new Province();
+					province.setProvinceCode(array[0]);
+					province.setProvinceName(array[1]);
+					simpleWeatherDB.saveProvince(province);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean handlerCitiesResponse(SimpleWeatherDB simpleWeatherDB,
+			String response,int provinceId){
+		if(!TextUtils.isEmpty(response)){
+			String[] allCities = response.split(",");
+			if(allCities!=null&&allCities.length>0){
+				for(String c:allCities){
+					String[] array  = c.split("\\|");
+					City city = new City();
+					city.setCityCode(array[0]);
+					city.setCityName(array[1]);
+					city.setProvinceId(provinceId);
+					simpleWeatherDB.saveCity(city);
+				}
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static boolean handlerCountiesResponse(SimpleWeatherDB simpleWeatherDB,
+			String response,int cityId){
+		if(!TextUtils.isEmpty(response)){
+			String[] allCounties = response.split(",");
+			if(allCounties!=null&&allCounties.length>0){
+				for(String c:allCounties){
+					String[] array = c.split("\\|");
+					County county = new County();
+					county.setCountyCode(array[0]);
+					county.setCountyName(array[1]);
+					county.setCityId(cityId);
+					simpleWeatherDB.saveCounty(county);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
